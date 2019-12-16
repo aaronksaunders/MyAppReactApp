7e5d8f45abbf5e723d32e6a1c80284e1e1bea3fc
Aaron Saunders <aaronksaunders@gmail.com>
2019-12-15 23:26:27 -0500

    capacitor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,55 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonIcon,
+  IonCard,
+} from "@ionic/react";
+import { camera, close } from "ionicons/icons";
+import React from "react";
+
+import { CameraResultType } from "@capacitor/core";
+import { useCamera, availableFeatures } from "@ionic/react-hooks/camera";
 
 const Home: React.FC = () => {
+  // camera hook!!
+  const { photo, getPhoto } = useCamera();
+
+  const takePicture = () => {
+    if (availableFeatures.getPhoto) {
+      getPhoto({
+        quality: 100,
+        allowEditing: false,
+        resultType: CameraResultType.DataUrl
+      });
+    }
+  };
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Ionic Blank</IonTitle>
+
+          <IonButtons slot="end">
+            <IonButton onClick={() => takePicture()}>
+              <IonIcon icon={camera}></IonIcon>
+            </IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+        {photo ? (
+          <div>
+            <pre>{JSON.stringify(photo, null, 2)}</pre>
+            <IonCard>
+              <img src={photo.dataUrl || photo.webPath} />
+            </IonCard>
+          </div>
+        ) : null}
       </IonContent>
     </IonPage>
   );
